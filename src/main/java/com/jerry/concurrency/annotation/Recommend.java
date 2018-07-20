a2292978b82f558ccb69e850542de3b17cce1b36
JerryTan <thrforever@126.com>
2018-07-20 15:25:14 +0800

    3-2 案例准备工作

---
+++ src/main/java/com/jerry/concurrency/annotation/Recommend.java
@@ -0,0 +1,21 @@
+package com.jerry.concurrency.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Jerry
+ * Date: 2018/7/20
+ * Time: 15:19
+ * Description: 用来标记【推荐】的类或者写法
+ */
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface Recommend {
+
+    String value() default "";
+}
