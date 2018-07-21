2a61235022999f4e871e74f69bc05c436bf682d8
JerryTan <thrforever@126.com>
2018-07-21 14:54:06 +0800

    3-4 并发模拟-代码

---
+++ src/main/java/com/jerry/concurrency/test/ConcurrencyTest.java
@@ -0,0 +1,60 @@
+package com.jerry.concurrency.test;
+
+import com.jerry.concurrency.annotation.NotThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.Executor;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Jerry
+ * Date: 2018/7/21
+ * Time: 14:41
+ * Description:
+ */
+
+@Slf4j
+@NotThreadSafe
+public class ConcurrencyTest {
+
+    /**
+     * 请求总数
+     */
+    public static int clientTotal = 5000;
+
+    /**
+     * 同时并发执行的线程数
+     */
+    public static int threadTotal = 200;
+
+    public static int count = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                } catch (Exception e) {
+                    log.error("exception:{}", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("count:{}" + count);
+    }
+
+    private static void add() {
+        count++;
+    }
+}
